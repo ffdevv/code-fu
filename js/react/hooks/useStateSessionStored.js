1bc891f15baf966f74f3239aab7aab4af5a30ed3
ffdevv <97543560+ffdevv@users.noreply.github.com>
2022-03-29 12:28:12 +0200

    Create useStateSessionStored.js

---
+++ js/react/hooks/useStateSessionStored.js
@@ -0,0 +1,20 @@
+/*
+  Persists the state into a sessionStorage location
+  The state will persist until the browser session is closed or
+  until sessionStorage.clear() or sessionStorage.removeItem(sessionStorageKey)
+  are called.
+*/
+import { useState, useEffect } from "react";
+
+export default function useStateSessionStored(sessionStorageKey, initValue) {
+  const [value, setValue] = useState(
+    sessionStorage.getItem(sessionStorageKey) || initValue
+  );
+  
+  useEffect(() => {
+    sessionStorage.setItem(sessionStorageKey, value);
+    return () => sessionStorage.removeItem(sessionStorageKey);
+  }, [value, sessionStorageKey]);
+
+  return [value, setValue];
+}
