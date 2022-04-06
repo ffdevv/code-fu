a637de81da924a91915b4e06a221749c27b4afaf
ffdevv <97543560+ffdevv@users.noreply.github.com>
2022-04-06 08:51:34 +0200

    Update regexps.js

--- rx/regexps.js
+++ rx/regexps.js
@@ -31,3 +31,10 @@ export const PASSWORD_FORMAT_MIXED_SPECIAL = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA
  * length of 3+ (last number)
 */
 export const USERNAME_SIMPLE = /^[a-zA-Z0-9_.-]{3,}$/
+
+
+/**
+ * Ip v4 format
+ *
+*/
+export const IPV4 = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])(?=\s|$)/
