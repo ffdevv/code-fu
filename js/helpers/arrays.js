3991f4b43cec8a68d78fca0d1d02f4fe5512de9f
ffdevv <97543560+ffdevv@users.noreply.github.com>
2022-06-09 11:21:09 +0200

    Update arrays.js

--- js/helpers/arrays.js
+++ js/helpers/arrays.js
@@ -1,2 +1,2 @@
-const max = (arr) => arr.reduce((acc,itm) => itm > acc ? itm : acc);
-const min = (arr) => arr.reduce((acc,itm) => itm < acc ? itm : acc);
+export const max = (arr) => arr.reduce((acc,itm) => itm > acc ? itm : acc);
+export const min = (arr) => arr.reduce((acc,itm) => itm < acc ? itm : acc);
