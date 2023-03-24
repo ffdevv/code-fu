ff2b6e6dfb7ef39733c65cebf0b0530e428f90cd
ffdevv <97543560+ffdevv@users.noreply.github.com>
2023-03-24 11:01:19 +0100

    Create config.py

---
+++ python/classes/config.py
@@ -0,0 +1,53 @@
+# pip install python-dotenv
+from dotenv import dotenv_values
+import json
+
+class Config:
+  """Represents a configuration for a program.
+
+  The Config class provides a way to store and access configuration options
+  for a program. The configuration options are loaded from a file, which can
+  be either a .env file or a JSON file.
+
+  Attributes:
+      DEFAULTS (dict): A dictionary that contains the default values for
+          the configuration options.
+      
+  Methods:
+      __init__(filepath: str = '.env'): Initializes a new Config object.
+          filepath (str): The path to the file containing the configuration
+              values. If the file is a .env file, the values are loaded using
+              the dotenv_values function from the dotenv module. If the file
+              is a JSON file, the values are loaded using the json.load
+              function. If no filepath is provided, the default value is
+              '.env'.
+  """
+
+  # Define the default values for the configuration options
+  DEFAULTS = {
+    # Ellipsis are mandatory without a default
+    "dbHost": Ellipsis,
+    "dbPort": Ellipsis,
+    "dbUsername": Ellipsis,
+    "dbPassword": Ellipsis
+  }
+
+  def __init__(self, filepath: str = '.env'):
+    # Check if the provided filepath is a .env or .json file, and load the values accordingly
+    if filepath.endswith('.env'):
+      values = dotenv_values(filepath)
+    elif filepath.endswith('.json'):
+      with open(filepath, 'r', encoding='utf8') as fi:
+        values = json.load(fi)
+    else:
+      raise TypeError("Unsupported filepath: provide a .env or .json file")
+
+    # Set the configuration options for the object using the loaded values
+    for k, v in self.DEFAULTS.items():
+      v1 = values.get(k, v)
+      if v1 == Ellipsis:
+        raise KeyError("config file missing mandatory field: " + k)
+      setattr(self, k, v1)
+
+  def __repr__(self):
+    return f"Config<{self.__dict__}>"
