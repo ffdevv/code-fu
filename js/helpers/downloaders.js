2d5c6c79007b681c6221bf71f38949496a7456af
ffdevv <97543560+ffdevv@users.noreply.github.com>
2023-04-03 11:53:04 +0200

    Update downloaders.js

--- js/helpers/downloaders.js
+++ js/helpers/downloaders.js
@@ -14,3 +14,27 @@ const blobToB64 = async (blob, trimMetadata = true) => {
 
   return trimMetadata ? result.substr(result.indexOf(",") + 1) : result;
 };
+
+const downloadFile = function (fileName, fileContent) {
+  var textFile = null,
+      makeFile = function (content) {
+        var data = new Blob([content], {type: 'text/plain'});
+        // If we are replacing a previously generated file we need to
+        // manually revoke the object URL to avoid memory leaks.
+        if (textFile !== null) {
+          window.URL.revokeObjectURL(textFile);
+        }
+        textFile = window.URL.createObjectURL(data);
+        return textFile;
+      };
+
+  var link = $("<a></a>");
+  link.css('visibility','hidden');
+  link.attr("download",fileName);
+  link.attr("href", makeFile(fileContent));
+  $('body').append(link);
+  var e = document.createEvent('MouseEvents');
+  e.initEvent( 'click', true, true );
+  link[0].dispatchEvent(e);
+  link.remove();
+};
