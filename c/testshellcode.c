f49ea2248de5433c229529c79061437b5d545ed3
ffdevv <97543560+ffdevv@users.noreply.github.com>
2023-07-17 14:00:48 +0200

    Create testshellcode.c

---
+++ c/testshellcode.c
@@ -0,0 +1,16 @@
+// gcc -m32 -g -fno-stack-protector -z execstack testshc.c -o testshc -w
+#include <sys/mman.h>
+#include <errno.h>
+
+const char shellcode[] = "";
+
+void main() {
+  char *buf;
+  int prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+  int flags = MAP_PRIVATE | MAP_ANONYMOUS;
+
+  buf = mmap(0, sizeof(shellcode), prot, flags, -1, 0);
+  memcpy(buf, shellcode, sizeof(shellcode));
+
+  ((void (*)(void))buf)();
+}
