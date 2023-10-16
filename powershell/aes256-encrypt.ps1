33e039809010c172a1c59611d1e03200eba25c07
ffdevv <97543560+ffdevv@users.noreply.github.com>
2023-10-16 16:36:22 +0200

    Update aes256-encrypt.ps1

--- powershell/aes256-encrypt.ps1
+++ powershell/aes256-encrypt.ps1
@@ -2,51 +2,67 @@ function AES256-Encrypt {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$false, ValueFromPipeline=$true)]
-        [string]$Data,
+        [byte[]]$Data,
 
         [Parameter(Mandatory=$false)]
         [string]$InputFile,
 
         [Parameter(Mandatory=$false)]
-        [string]$Base64Key,
+        [string]$KeyBase64,
+        [Parameter(Mandatory=$false)]
+        [byte[]]$KeyBytes,
 
         [Parameter(Mandatory=$false)]
-        [string]$Base64IV
+        [string]$IVBase64,
+        [Parameter(Mandatory=$false)]
+        [byte[]]$IVBytes
     )
-
-    process {
+    
+    begin {
         # Check if only Base64Key is provided without Base64IV
-        if ($Base64Key -xor $Base64IV) {
-            throw "Error: Either provide both Base64Key and Base64IV or none of them."
+        if (($KeyBase64 -xor $KeyBytes) -or ($KeyBytes -xor $IVBytes)) {
+            throw "Error: Either provide both Key and IV (or Base64 version) or none of them."
         }
-
+        # Create a list to hold data from the pipeline
+        $dataList = @()
+    }
+    
+    process {
+        # If data is provided from the pipeline, add it to the dataList
+        if ($Data) {
+            $dataList += $Data
+        }
+    }
+    
+    end {
         # Read code from file or use the code provided via parameter or pipeline
-        $data = if ($InputFile) {
-            Get-Content -Path $InputFile -Raw
-        } elseif ($Data) {
-            $Data
+        $dataBytes = if ($dataList.Count -gt 0) {
+            $dataList
+        } elseif ($InputFile) {
+            [System.IO.File]::ReadAllBytes($InputFile)
         } else {
             throw "Error: Either provide data through the pipeline, the Data parameter, or specify an InputFile."
         }
-
-        # Convert the code to a byte array
-        $dataBytes = [System.Text.Encoding]::UTF8.GetBytes($data)
 
         # Create an AES object
         $aes = [System.Security.Cryptography.Aes]::Create()
 
         # If Base64Key and Base64IV are provided, use them; otherwise, generate new ones
-        if ($Base64Key -and $Base64IV) {
-            $aes.Key = [Convert]::FromBase64String($Base64Key)
-            $aes.IV = [Convert]::FromBase64String($Base64IV)
-        } else {
+        if ($KeyBase64 -and $IVBase64) {
+            $aes.Key = [Convert]::FromBase64String($KeyBase64)
+            $aes.IV  = [Convert]::FromBase64String($IVBase64)
+        } elseif ($KeyBytes -and $IVBytes) {
+            $aes.Key = $KeyBytes
+            $aes.IV  = $IVBytes
+        }
+        else {
             $aes.GenerateKey()
             $aes.GenerateIV()
         }
 
         # Encrypt the code
         $encryptor = $aes.CreateEncryptor($aes.Key, $aes.IV)
-        $encryptedBytes = $encryptor.TransformFinalBlock($dataBytes, 0, $codeBytes.Length)
+        $encryptedBytes = $encryptor.TransformFinalBlock($dataBytes, 0, $dataBytes.Length)
 
         # Create an object to hold the Key, IV, and Encrypted data
         $result = [PSCustomObject]@{
@@ -61,7 +77,7 @@ function AES256-Encrypt {
 }
 
 # Example usage:
-# $encrypted = "Write-Host 'Hello, World!'" | Encrypt-CodeBlock
+# $encrypted = [System.Text.Encoding]::UTF8.GetBytes("Write-Host 'Hello, World!'") | AES256-Encrypt
 # $encrypted | Format-List
 # [System.Convert]::ToBase64String($encrypted.Key)
 # [System.Convert]::ToBase64String($encrypted.IV)
